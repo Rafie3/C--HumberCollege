c79ac618b3310303dd6657c97ed32a5f255bdd26
Rafie Nargis <56384816+Rafie3@users.noreply.github.com>
2023-04-17 19:53:26 -0400

    Question 05
    Write classes for events, one class which exposes an event and another which handles the event.
    Choose any scenario you prefer to build your code.
    
    1)   Create a class to pass as an argument for the event handlers (EventArgs class)
    2)   Set up the delegate for the event
    3)   Declare the code for the event
    4)   Create code the will be run when the event occurs (Event Handler)
    5)   Associate the event with the event handler

---
+++ TemperatureChangedEventArgs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class TemperatureChangedEventArgs : EventArgs
+{
+    public double CurrentTemperature { get; set; }
+}
+
+public delegate void TemperatureChangedEventHandler(object sender, TemperatureChangedEventArgs e);
+
+public class WeatherStation
+{
+    public event TemperatureChangedEventHandler TemperatureChanged;
+
+    private double _currentTemperature;
+
+    public double CurrentTemperature
+    {
+        get { return _currentTemperature; }
+        set
+        {
+            if (value != _currentTemperature)
+            {
+                _currentTemperature = value;
+                OnTemperatureChanged(new TemperatureChangedEventArgs { CurrentTemperature = _currentTemperature });
+            }
+        }
+    }
+
+    protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
+    {
+        TemperatureChanged?.Invoke(this, e);
+    }
+}
+
+public class TemperatureDisplay
+{
+    public void Subscribe(WeatherStation weatherStation)
+    {
+        weatherStation.TemperatureChanged += WeatherStation_TemperatureChanged;
+    }
+
+    private void WeatherStation_TemperatureChanged(object sender, TemperatureChangedEventArgs e)
+    {
+        Console.WriteLine($"The current temperature is {e.CurrentTemperature} degrees Celsius.");
+    }
+}
